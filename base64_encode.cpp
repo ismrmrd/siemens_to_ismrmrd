3ada74f18bee48332ec9311467f42b444a5fd4b5
Dusan Puletic <dusan.puletic@nih.gov>
2014-07-28 11:54:07 -0400

    embeded strings split into multiple lines (windows issue resolved)

--- base64_encode.cpp
+++ base64_encode.cpp
@@ -10,55 +10,79 @@ static const std::string base64_chars =
              "0123456789+/";
 
 
-static inline bool is_base64(unsigned char c) {
-  return (isalnum(c) || (c == '+') || (c == '/'));
+static inline bool is_base64(unsigned char c)
+{
+	return (isalnum(c) || (c == '+') || (c == '/'));
 }
 
-std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
-  std::string ret;
-  int i = 0;
-  int j = 0;
-  unsigned char char_array_3[3];
-  unsigned char char_array_4[4];
+std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len)
+{
+	std::string ret;
+	int i = 0;
+	int j = 0;
+	unsigned char char_array_3[3];
+	unsigned char char_array_4[4];
 
-  while (in_len--) {
-    char_array_3[i++] = *(bytes_to_encode++);
-    if (i == 3) {
-      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-      char_array_4[3] = char_array_3[2] & 0x3f;
+	while (in_len--)
+	{
+		char_array_3[i++] = *(bytes_to_encode++);
+		if (i == 3)
+		{
+			char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+			char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+			char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+			char_array_4[3] = char_array_3[2] & 0x3f;
 
-      for(i = 0; (i <4) ; i++)
-        ret += base64_chars[char_array_4[i]];
-      i = 0;
+			for(i = 0; (i <4) ; i++)
+				ret += base64_chars[char_array_4[i]];
+			i = 0;
+		}
+	}
+
+	if (i)
+	{
+		for(j = i; j < 3; j++)
+			char_array_3[j] = '\0';
+
+		char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+		char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+		char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+		char_array_4[3] = char_array_3[2] & 0x3f;
+
+		for (j = 0; (j < i + 1); j++)
+			ret += base64_chars[char_array_4[j]];
+
+		while((i++ < 3))
+			ret += '=';
+	}
+	return ret;
+}
+
+
+void splitBigString(std::ofstream &resFile, std::string bigString, int parts)
+{
+	int len = bigString.length();
+
+	int at, pre=0, i;
+
+	for (pre = i = 0; i < parts; ++i)
+    {
+		at = (len + len*i)/parts;
+		if (parts-i == 1)
+		{
+			resFile << "\""<< bigString.substr(pre, at-pre) << "\");" << std::endl;
+		}
+		else
+		{
+			resFile << "\""<< bigString.substr(pre, at-pre) << "\"" << std::endl;
+		}
+		pre = at;
     }
-  }
-
-  if (i)
-  {
-    for(j = i; j < 3; j++)
-      char_array_3[j] = '\0';
-
-    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-    char_array_4[3] = char_array_3[2] & 0x3f;
-
-    for (j = 0; (j < i + 1); j++)
-      ret += base64_chars[char_array_4[j]];
-
-    while((i++ < 3))
-      ret += '=';
-
-  }
-
-  return ret;
-
 }
 
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
 	std::string infile_xml_VB(argv[1]);
 	std::string infile_xml_VD(argv[2]);
@@ -86,7 +110,26 @@ int main(int argc, char *argv[]) {
 
 	std::ofstream ofs1 (encoded_file.c_str(), std::ofstream::out);
 
-	ofs1 << "#include <iostream>" << std::endl << std::endl << "std::string global_xml_VB_string(\"" << encoded_xml_VB << "\");" << std::endl << "std::string global_xml_VD_string(\"" << encoded_xml_VD << "\");" << std::endl << "std::string global_xsl_string(\"" << encoded_xsl << "\");" << std::endl << "std::string global_xsd_string(\"" << encoded_xsd << "\");" << std::endl;
+	ofs1 << "#include <iostream>" << std::endl << std::endl;
+
+	ofs1 << "std::string global_xml_VB_string(";
+
+	splitBigString(ofs1, encoded_xml_VB, 4);
+
+	ofs1 << "std::string global_xml_VD_string(";
+
+	splitBigString(ofs1, encoded_xml_VD, 4);
+
+	ofs1 << "std::string global_xsl_string(";
+
+	splitBigString(ofs1, encoded_xsl, 4);
+
+	ofs1 << "std::string global_xsd_string(";
+
+	splitBigString(ofs1, encoded_xsd, 4);
+
+	//ofs1 << "#include <iostream>" << std::endl << std::endl << "std::string global_xml_VB_string(\"" << encoded_xml_VB << "\");" << std::endl << "std::string global_xml_VD_string(\"" << encoded_xml_VD << "\");" << std::endl << "std::string global_xsl_string(\"" << encoded_xsl << "\");" << std::endl << "std::string global_xsd_string(\"" << encoded_xsd << "\");" << std::endl;
+
 	ofs1.close();
 
 	return 0;
