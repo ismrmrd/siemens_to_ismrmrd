7a9faca6bccc1ee2b0ef3c792649268c310f6b90
Dusan Puletic <dusan.puletic@nih.gov>
2014-11-24 20:46:09 +0000

    script for creating converter tar modified.

--- create_converter_tar.sh
+++ create_converter_tar.sh
@@ -2,17 +2,19 @@
 
 BASEDIR=$(dirname $0)
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 1 ]; then
   TAR_FILE_NAME=converter-`date '+%Y%m%d-%H%M'`
   mkdir $BASEDIR/dep_location
   mkdir $BASEDIR/tar_location
 
-  $BASEDIR/copy_converter_file_and_dependencies $BASEDIR/build/siemens_to_ismrmrd $BASEDIR/dep_location  
+  CONVERTER_BINARY=${1}
+
+  $BASEDIR/copy_converter_file_and_dependencies $CONVERTER_BINARY $BASEDIR/dep_location  
   cp $BASEDIR/LICENSE $BASEDIR/dep_location
   tar -zcf $BASEDIR/tar_location/$TAR_FILE_NAME.tar.gz --directory $BASEDIR/dep_location/ .
   rm -rf $BASEDIR/dep_location/
   exit 0
 else
-  echo -e "\nUsage:  $0\n"
+  echo -e "\nUsage:  $0 <converter binary>\n"
   exit 1
 fi
