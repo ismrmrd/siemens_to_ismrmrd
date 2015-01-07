3784eedbccd9dff08020ac874f098ad134a7dcbb
Dusan Puletic <dusan.puletic@nih.gov>
2015-01-07 17:26:39 +0000

    Added versioning and packaging

---
+++ cmake/siemens_to_ismrmrd_cpack.cmake
@@ -0,0 +1,32 @@
+################################################################################
+# Find available package generators
+################################################################################
+
+if(UNIX)
+  # DEB
+  find_program(DPKG_PROGRAM dpkg)
+  if(EXISTS ${DPKG_PROGRAM})
+    list(APPEND CPACK_GENERATOR "DEB")
+  endif(EXISTS ${DPKG_PROGRAM})
+  # TODO: RPM
+  #find_program(RPMBUILD_PROGRAM rpmbuild)
+  #if(EXISTS ${RPMBUILD_PROGRAM})
+  #  list(APPEND CPACK_GENERATOR "RPM")
+  #endif(EXISTS ${RPMBUILD_PROGRAM})
+endif(UNIX)
+
+if(WIN32)
+    # NSLS
+    list(APPEND CPACK_GENERATOR "NSIS")    
+endif(WIN32)
+
+# TODO: list(APPEND CPACK_SOURCE_GENERATOR "TGZ")
+# TODO: list(APPEND CPACK_SOURCE_GENERATOR "ZIP")
+list(APPEND CPACK_SOURCE_IGNORE_FILES ";.git;.gitignore;todo.txt;_clang-format;build/")
+
+# set dependencies explicitly
+include(InstallRequiredSystemLibraries)
+set(DEBIAN_PACKAGE_DEPENDS "ismrmrd, libxml2-dev, libxslt-dev, libboost-system-dev, libboost-thread-dev, libboost-program-options-dev, libboost-filesystem-dev")
+
+# where the package metadata are
+set(SIEMENS_TO_ISMRMRD_CPACK_CFG_FILE "${PROJECT_BINARY_DIR}/cpack_options.cmake")
