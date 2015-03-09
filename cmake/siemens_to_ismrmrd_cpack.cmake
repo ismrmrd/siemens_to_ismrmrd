0fcbeaac244f8e7a1a8f4615bca2d1dabc9d2267
Dusan Puletic <dusan.puletic@nih.gov>
2015-03-09 16:34:36 +0000

    packaging: cpack used, no components

--- cmake/siemens_to_ismrmrd_cpack.cmake
+++ cmake/siemens_to_ismrmrd_cpack.cmake
@@ -10,16 +10,27 @@ if(UNIX)
   endif(EXISTS ${DPKG_PROGRAM})
 endif(UNIX)
 
-if(WIN32)
-    # NSLS
-    list(APPEND CPACK_GENERATOR "NSIS")    
-endif(WIN32)
+# Enable/Disable automatic search for dependencies:
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 
-list(APPEND CPACK_SOURCE_IGNORE_FILES ";.git;.gitignore;todo.txt;_clang-format;build/")
+# Set dependencies explicitly
+set(DEBIAN_PACKAGE_DEPENDS "ismrmrd, libxml2-dev, libxslt-dev, libboost-system-dev, libboost-thread-dev, libboost-program-options-dev, libboost-filesystem-dev")
 
-# where the package metadata are
+# Where the package metadata are
 set(SIEMENS_TO_ISMRMRD_CPACK_CFG_FILE "${PROJECT_BINARY_DIR}/cpack_options.cmake")
 
 # Where the package to be installed 
 set(CPACK_PACKAGING_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
 message("CPACK_PACKAGING_INSTALL_PREFIX: " ${CPACK_PACKAGING_INSTALL_PREFIX})
+
+if(WIN32)
+    # NSLS
+    list(APPEND CPACK_GENERATOR "NSIS")    
+endif(WIN32)
+
+list(APPEND CPACK_SOURCE_GENERATOR "TGZ")
+list(APPEND CPACK_SOURCE_GENERATOR "ZIP")
+list(APPEND CPACK_SOURCE_IGNORE_FILES ";.git;.gitignore;todo.txt;_clang-format;build/")
+
+# Set dependencies explicitly
+include(InstallRequiredSystemLibraries)
